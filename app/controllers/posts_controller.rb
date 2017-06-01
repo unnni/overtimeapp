b5e94dc3ad14494e4f1c6356852177bcb6567efe
unnni <r.unnikrishnan@outlook.com>
2017-06-01 20:04:13 +0530

    index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 	before_action :set_post, only: [:show]
 	
 	def index
+		@posts = Post.all
 	end
 
 	def new
@@ -10,6 +11,7 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
+		@post.user_id = current_user.id.to_i
 
 		if @post.save
 			redirect_to @post, notice: 'Your post was created successfully'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name
+  has_many :posts
+
+  def full_name
+  	last_name.upcase + ", " + first_name.upcase
+  end
+  
+
 end
