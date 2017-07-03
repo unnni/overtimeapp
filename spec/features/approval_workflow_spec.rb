f2a86d59fb5adecf94215df6f61b2080dbc3740b
unnni <r.unnikrishnan@outlook.com>
2017-07-03 23:46:32 +0530

    added permissions only for admin users to change status

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -2,23 +2,33 @@ require 'rails_helper'
 
 describe 'navigate' do
 	before do
-		@adminuser = FactoryGirl.create(:admin_user)
+		@admin_user = FactoryGirl.create(:admin_user)
 		login_as(@admin_user, :scope => :user)
 	end
 
 	describe 'edit' do	
 		before do
 			@post = FactoryGirl.create(:post)
+			visit edit_post_path(@post)
 		end
 
-		#it 'has a status that can be edited on the form' do
-		#	visit edit_post_path(@post)
-			#page.choose('post_status_approved', visible: false)
+		it 'has a status that can be edited on the form' do
+			choose('post_status_approved')
 		#	find(:id => 'post_status_approved').choose('Approved')
-		#	click_on "Save"
-		#	expect(@post.reload.status).to eq('approved')
-		#end
+			click_on "Save"
+			expect(@post.reload.status).to eq('approved')
+		end
 
 		
+
+		it 'cannot be edited by a non admin' do
+			logout(:user)
+			user = FactoryGirl.create(:user)
+			login_as(user, :scope => :user)
+
+			visit edit_post_path(@post)
+
+			expect(page).to_not have_content('Approved')
+		end	
 	end
 end
